b37458ab0a061e792029e386bc3351dc2d56e611
FelipeOrtiz111 <101360596+FelipeOrtiz111@users.noreply.github.com>
2024-10-03 16:13:04 -0300

    Carpeta "users" creada.
     la cual contiene lógica para la confirmación de correo al inicio de sesión

--- Plataforma_Ingles/Pagina_Web_Ingles/views.py
+++ Plataforma_Ingles/Pagina_Web_Ingles/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 # Create your views here.
+
+def sign_up(request):
+    return render(request, 'sign_up.html')
