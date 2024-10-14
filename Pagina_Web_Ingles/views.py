7b21d3a19e8fec2a87e4f5632b0901c226e574bf
Crissxs <cri.carrasco@duocuc.cl>
2024-10-13 22:05:48 -0300

    Fue implementada a Url y Views la pagina de perfil, tambien se cambio el diseño de la pagina principal y se añadio un boton para realizar validaciones a los formularios de inicio de sesion

--- Pagina_Web_Ingles/views.py
+++ Pagina_Web_Ingles/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def perfil_view(request):
+    return render(request, 'perfil.html')
 # Create your views here.
