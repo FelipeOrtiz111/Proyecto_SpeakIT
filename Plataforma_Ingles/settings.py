0feb5c7f9679e2dcc4bb5e223aaa7a6a49cf9a4d
Crissxs <cri.carrasco@duocuc.cl>
2024-10-15 22:09:59 -0300

    Cambio de logica en las plantillas HTML en la que se aplica extends e includes para trabajar con mas facilidad el codigo, header añadido a las vistas y se puede utilizar en cualquier pagina con {% extends "header.html" %}.

--- Plataforma_Ingles/settings.py
+++ Plataforma_Ingles/settings.py
@@ -68,8 +68,7 @@ ROOT_URLCONF = 'Plataforma_Ingles.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        #'DIRS': [os.path.join(BASE_DIR, 'Pagina_Web_Ingles', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Pagina_Web_Ingles', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
