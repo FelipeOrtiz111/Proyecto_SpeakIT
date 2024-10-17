37f22fb6d50b643b2768520a34086b072eb2e3b0
FelipeOrtiz111 <101360596+FelipeOrtiz111@users.noreply.github.com>
2024-10-17 11:24:58 -0300

    quedó listo el inicio de sesión
    Se aplicó la confirmación por correo electrónico más la validación del correo institucional.

--- users/views.py
+++ users/views.py
@@ -1,22 +1,63 @@
+from typing import Protocol
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth import login, logout, authenticate, get_user_model
 from django.contrib import messages
-from .forms import UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.core.mail import EmailMessage
 
 from .forms import UserRegistrationForm, UserLoginForm
 from .decorators import user_not_authenticated
+from .tokens import account_activation_token
 
-# Create your views here.
+def activate(request, uidb64, token):
+    User = get_user_model()
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except:
+        user = None
+
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+
+        messages.success(request, "Gracias por confirmar tu email. Ahora puedes ingresar a tu cuenta.")
+        return redirect('login')
+    else:
+        messages.error(request, "Activation link is invalid!")
+
+    return redirect('homepage')
+
+def activateEmail(request, user, to_email):
+    mail_subject = "[SpeakIT] Activa tu cuenta de usuario."
+    message = render_to_string("template_activate_account.html", {
+        'user': user.username,
+        'domain': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+        "protocol": 'https' if request.is_secure() else 'http'
+    })
+    email = EmailMessage(mail_subject, message, from_email='speakit.duocuc@gmail.com', to=[to_email])
+    if email.send():
+        messages.success(request, f'<b>{user}</b>, por favor ve a la bandeja de entrada de tu email <b>{to_email}</b> \
+                     y haz click en el link de confirmación para completar tu registro. <b>Nota:</b> Revisa tu carpeta de spam')
+    else:
+        messages.error(request, f'Ocurrió un problema enviando el correo de confirmación a {to_email}, Revisa si está escrito correctamente.')
+
 @user_not_authenticated
 def register(request):
     if request.method == "POST":
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, f"Nueva cuenta creada {user.username}")
-            return redirect('/')
+            user = form.save(commit=False)
+            user.is_active=False
+            user.save()
+            activateEmail(request, user, form.cleaned_data.get('email'))
+            return redirect('login')
 
         else:
             for error in list(form.errors.values()):
