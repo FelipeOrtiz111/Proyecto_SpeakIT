a5bd84bc45e505c632e82aa2b77975017ea61d08
Crissxs <cri.carrasco@duocuc.cl>
2024-10-18 03:57:13 -0300

    Static Root añadido a settings.py para definir una carpeta de recursos estaticos

--- Plataforma_Ingles/settings.py
+++ Plataforma_Ingles/settings.py
@@ -145,6 +145,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Pagina_Web_Ingles/static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
