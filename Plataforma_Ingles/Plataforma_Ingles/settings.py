f2c56d792fbc8aafaf5beb69eca7d7093eb9d489
Crissxs <cri.carrasco@duocuc.cl>
2024-10-18 03:34:11 -0300

    Espero que no sea tan tonto...

--- Plataforma_Ingles/Plataforma_Ingles/settings.py
+++ Plataforma_Ingles/Plataforma_Ingles/settings.py
@@ -141,12 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Pagina_Web_Ingles/static'),
+    os.path.join(BASE_DIR, 'static'),  # Si tienes archivos aquí
+    os.path.join(BASE_DIR, 'Pagina_Web_Ingles/static'),  # Si tienes archivos aquí
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
