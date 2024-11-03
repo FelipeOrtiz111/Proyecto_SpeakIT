3bac2fd77ed88b8b0ff55f5d358d27604b7e251d
Crissxs <cri.carrasco@duocuc.cl>
2024-11-03 20:54:22 -0300

    a?

--- Pagina_Web_Ingles/urls.py
+++ Pagina_Web_Ingles/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('header', views.includes, name='header'),
+    path('videos/<int:unidad_id>/', views.videos_por_unidad, name='videos_por_unidad'),
 ]   
--- Pagina_Web_Ingles/views.py
+++ Pagina_Web_Ingles/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Video
 
 def index(request):
     return render(request, 'index.html')
 
 def includes(request):
     return render(request, 'header.html')
+
+def videos_por_unidad(request, unidad_id):
+    videos = Video.objects.filter(unidad=unidad_id)
+    return render(request, 'includes/videos_list.html', {'videos': videos})
 # Create your views here.
