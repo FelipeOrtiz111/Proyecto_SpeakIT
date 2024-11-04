cbee10034acd07d00ef93f2ff3d0d1b0c26be761
Crissxs <cri.carrasco@duocuc.cl>
2024-11-03 22:58:32 -0300

    Cambio a los videos

--- Pagina_Web_Ingles/static/scripts/sidenav.js
+++ Pagina_Web_Ingles/static/scripts/sidenav.js
@@ -17,3 +17,26 @@ function toggleDescription(element) {
   const videoSection = element.closest('.video-section');
   videoSection.classList.toggle('expanded');
 }
+
+const links = document.querySelectorAll('.dropdown-container a.dc-a');
+const videoSections = document.querySelectorAll('.video-section');
+
+links.forEach(link => {
+    link.addEventListener('click', function() {
+        // Limpia las selecciones anteriores
+        links.forEach(item => item.classList.remove('active'));
+        videoSections.forEach(video => video.style.display = 'none');
+
+        // Marca el enlace como activo
+        this.classList.add('active');
+
+        // Muestra el video correspondiente
+        const selectedTitle = this.getAttribute('data-titulo');
+        videoSections.forEach(video => {
+            const title = video.querySelector('h3').innerText;
+            if (title === selectedTitle) {
+                video.style.display = 'block';
+            }
+        });
+    });
+});
