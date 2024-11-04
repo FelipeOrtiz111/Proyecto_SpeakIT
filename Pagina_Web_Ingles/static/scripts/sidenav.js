99f154eb76926fda5e1a63a765d712e61eccbc28
Crissxs <cri.carrasco@duocuc.cl>
2024-11-03 23:23:02 -0300

    Estilo de Videos

--- Pagina_Web_Ingles/static/scripts/sidenav.js
+++ Pagina_Web_Ingles/static/scripts/sidenav.js
@@ -12,31 +12,50 @@ for (i = 0; i < dropdown.length; i++) {
     }
   });
 }
+
 // Activa los contenedores de los videos
 function toggleDescription(element) {
-  const videoSection = element.closest('.video-section');
-  videoSection.classList.toggle('expanded');
+    const videoSection = element.closest('.video-section');
+    const videoContainer = videoSection.querySelector('.video-container');
+    
+    // Alterna la visualización del video
+    if (videoContainer.style.display === "block") {
+        videoContainer.style.display = "none";
+    } else {
+        videoContainer.style.display = "block";
+    }
 }
 
 const links = document.querySelectorAll('.dropdown-container a.dc-a');
 const videoSections = document.querySelectorAll('.video-section');
 
 links.forEach(link => {
-    link.addEventListener('click', function() {
-        // Limpia las selecciones anteriores
-        links.forEach(item => item.classList.remove('active'));
-        videoSections.forEach(video => video.style.display = 'none');
+  link.addEventListener('click', function() {
+      // Limpia las selecciones anteriores
+      links.forEach(item => item.classList.remove('active'));
+      videoSections.forEach(video => video.style.display = 'none');
 
-        // Marca el enlace como activo
-        this.classList.add('active');
+      // Marca el enlace como activo
+      this.classList.add('active');
 
-        // Muestra el video correspondiente
-        const selectedTitle = this.getAttribute('data-titulo');
-        videoSections.forEach(video => {
-            const title = video.querySelector('h3').innerText;
-            if (title === selectedTitle) {
-                video.style.display = 'block';
-            }
-        });
-    });
+      // Muestra el video correspondiente
+      const selectedTitle = this.getAttribute('data-titulo');
+      let videoFound = false; // Variable para rastrear si se encontró un video
+
+      videoSections.forEach(video => {
+          const title = video.querySelector('h3').innerText;
+          if (title === selectedTitle) {
+              video.style.display = 'block';
+              videoFound = true; // Se encontró un video
+          }
+      });
+
+      // Muestra u oculta el mensaje "No se encuentra video"
+      const noVideoMessage = document.querySelector('.no-video');
+      if (videoFound) {
+          noVideoMessage.style.display = 'none'; // Oculta el mensaje si se encontró un video
+      } else {
+          noVideoMessage.style.display = 'block'; // Muestra el mensaje si no se encontró video
+      }
+  });
 });
