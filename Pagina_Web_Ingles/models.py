1bd9d8fc00cd9b619de08d28a626a59fb40b1aa3
Crissxs <cri.carrasco@duocuc.cl>
2024-11-03 22:02:15 -0300

    Normalidad

--- Pagina_Web_Ingles/models.py
+++ Pagina_Web_Ingles/models.py
@@ -1,14 +1,5 @@
 from django.db import models
 from django.utils import timezone
-# Create your models here.
-class Video(models.Model):
-    titulo = models.CharField(max_length=200)
-    descripcion = models.TextField()
-    enlace = models.URLField()
-    miniatura = models.URLField()
-    unidad = models.ForeignKey('Unidad', on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return self.titulo
 
+# Create your models here.
 
--- Pagina_Web_Ingles/urls.py
+++ Pagina_Web_Ingles/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('header', views.includes, name='header'),
-    path('videos/<int:unidad_id>/', views.videos_por_unidad, name='videos_por_unidad'),
 ]   
