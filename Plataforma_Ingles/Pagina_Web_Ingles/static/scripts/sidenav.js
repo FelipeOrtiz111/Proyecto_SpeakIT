93936a280375986cc0098b4e3b82d13da56a7289
Crissxs <cri.carrasco@duocuc.cl>
2024-11-03 22:00:57 -0300

    Revert "Actualizacion de videos por ajax"
    This reverts commit 62203ac6134c89d73a02efe13ed86cb940c7c88f.

--- Plataforma_Ingles/Pagina_Web_Ingles/static/scripts/sidenav.js
+++ Plataforma_Ingles/Pagina_Web_Ingles/static/scripts/sidenav.js
@@ -1,21 +1,6 @@
 var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
-document.querySelectorAll('.dc-a').forEach(link => {
-  link.addEventListener('click', function(event) {
-      event.preventDefault();
-      const unidadId = this.getAttribute('data-unidad');  // Obtén el ID de la unidad
-
-      // Realiza la solicitud AJAX para cargar los videos de la unidad seleccionada
-      fetch(`/videos/${unidadId}/`)
-          .then(response => response.text())
-          .then(html => {
-              document.querySelector('.video-section-container').innerHTML = html;
-          })
-          .catch(error => console.error('Error al cargar los videos:', error));
-  });
-});
-
 for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function() {
     this.classList.toggle("active");
