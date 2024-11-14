363eaafc34ad20c80ed5efbcf4fb2f9508563415
FelipeOrtiz111 <101360596+FelipeOrtiz111@users.noreply.github.com>
2024-11-14 18:01:45 -0300

    prueba corrección de errores

--- Plataforma_Ingles/Plataforma_Ingles/settings.py
+++ Plataforma_Ingles/Plataforma_Ingles/settings.py
@@ -166,7 +166,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Pagina_Web_Ingles/static', 'quizes/static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Pagina_Web_Ingles/static'),]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
