ab4f93e10f6ea70d934bdf2c46df87875d5ade4f
FelipeOrtiz111 <101360596+FelipeOrtiz111@users.noreply.github.com>
2024-11-14 22:48:54 -0300

    python migrate

--- Plataforma_Ingles/results/models.py
+++ Plataforma_Ingles/results/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from Pagina_Web_Ingles.models import Quiz
 from django.contrib.auth.models import User
+from Plataforma_Ingles import settings
 
 # Create your models here.
 
 class Result(models.Model):
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     score = models.FloatField()
 
     def __str__(self):
