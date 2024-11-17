50148b62f233f74f479b618dac0b61fafc910cfa
FelipeOrtiz111 <101360596+FelipeOrtiz111@users.noreply.github.com>
2024-11-16 21:03:46 -0300

    ver contenido de preguntas en consola

--- Plataforma_Ingles/Pagina_Web_Ingles/static/scripts/quiz.js
+++ Plataforma_Ingles/Pagina_Web_Ingles/static/scripts/quiz.js
@@ -1,1 +1,15 @@
 console.log("Hola");
+const url = window.location.href;
+console.log(url);
+
+
+$.ajax({
+    type: "GET",
+    url: `{url}data`,
+    success: function(response){
+        console.log(response);
+    },
+    error: function(error){
+        console.log(error)
+    }
+})
