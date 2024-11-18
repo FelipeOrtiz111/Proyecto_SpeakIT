6902465d383ed3356d7094098b446d76ae367da7
FelipeOrtiz111 <101360596+FelipeOrtiz111@users.noreply.github.com>
2024-11-18 10:28:15 -0300

    avance quizes

--- Plataforma_Ingles/Pagina_Web_Ingles/static/scripts/quiz.js
+++ Plataforma_Ingles/Pagina_Web_Ingles/static/scripts/quiz.js
@@ -1,36 +1,48 @@
-console.log("Hola");
+console.log("Quiz script loaded");
 const url = window.location.href;
-
 const quizBox = document.getElementById("quiz-box");
 
-$.ajax({
-    type: 'GET',
-    url: `${url}data`,
-    success: function(response){
-        // console.log(response);
-        data = response.data;
-        data.forEach(el => {
-            for (const [question, answers] of Object.entries(el)){
-                quizBox.innerHTML += `
-                    <hr>
-                    <div class="mb-2">
-                        <b>${question}</b>
-                    </div>
-                `;
+console.log("URL:", url);
 
-                answers.forEach(answer => {
+if (!quizBox) {
+    console.error("Quiz box element not found!");
+} else {
+    $.ajax({
+        type: 'GET',
+        url: `${url}data`,
+        success: function(response){
+            console.log("Response received:", response);
+            const data = response.data;
+            
+            if (!data || data.length === 0) {
+                console.log("No quiz data received");
+                quizBox.innerHTML = '<p>No hay preguntas disponibles para este quiz.</p>';
+                return;
+            }
+            
+            data.forEach(el => {
+                for (const [question, answers] of Object.entries(el)){
                     quizBox.innerHTML += `
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="${question}" id="${question}-${answer}" value="${answer}">
-                            <label class="form-check-label" for="${question}-${answer}">${answer}</label>
+                        <hr>
+                        <div class="mb-2">
+                            <b>${question}</b>
                         </div>
                     `;
-                });
-            }
-        });
-    },
-    error: function(error){
-        console.log(error);
-    }
-});
 
+                    answers.forEach(answer => {
+                        quizBox.innerHTML += `
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="${question}" id="${question}-${answer}" value="${answer}">
+                                <label class="form-check-label" for="${question}-${answer}">${answer}</label>
+                            </div>
+                        `;
+                    });
+                }
+            });
+        },
+        error: function(error){
+            console.error("Error fetching quiz data:", error);
+            quizBox.innerHTML = '<p>Error al cargar las preguntas del quiz.</p>';
+        }
+    });
+}
