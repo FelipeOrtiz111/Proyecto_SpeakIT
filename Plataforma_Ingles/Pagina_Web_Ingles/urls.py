dbe183db8c10e0a4251bf3c2261bfd09fbe16ffb
FelipeOrtiz111 <101360596+FelipeOrtiz111@users.noreply.github.com>
2024-11-18 12:57:59 -0300

    corrección archivo correcto

--- Plataforma_Ingles/Pagina_Web_Ingles/urls.py
+++ Plataforma_Ingles/Pagina_Web_Ingles/urls.py
@@ -15,7 +15,8 @@ urlpatterns = [
     path('header/', header_view, name='header'),
     path('quizes/', quizes_view, name='quizes'),
     path('seguimiento/', seguimiento_view, name='seguimiento'),
-    path('<pk>/', quiz_view, name='quiz-view'),
-    path('<pk>/data/', quiz_data_view, name='quiz-data-view'),
-    path('<pk>/save/', save_quiz_view, name='quiz-save-view'),
+    # URLs para el quiz
+    path('<int:pk>/', quiz_view, name='quiz-view'),
+    path('<int:pk>/data/', quiz_data_view, name='quiz-data-view'),
+    path('<int:pk>/save/', save_quiz_view, name='quiz-save-view'),
 ]   
