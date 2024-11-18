3b158e2642f610e5650ee5acde912fa8a6d8d297
FelipeOrtiz111 <101360596+FelipeOrtiz111@users.noreply.github.com>
2024-11-18 11:53:29 -0300

    avance quiz (views.py)

--- Plataforma_Ingles/Pagina_Web_Ingles/views.py
+++ Plataforma_Ingles/Pagina_Web_Ingles/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+
+from Plataforma_Ingles.results.models import Result
 from .models import Quiz
 from django.views.generic import ListView
 from django.http import JsonResponse
+from questions.models import Question, Answer
 
 def index(request):
     return render(request, 'index.html')
@@ -24,9 +27,11 @@ def quiz_view(request, pk):
     quiz = Quiz.objects.get(pk=pk)
     return render(request, 'quizes.html', {'obj': quiz})
 
+# Retorna los datos de las preguntas del quiz
 def quiz_data_view(request, pk):
     quiz = Quiz.objects.get(pk=pk)
     questions = []
+    # Obtener preguntas y respuestas de cada pregunta
     for q in quiz.get_questions():
         answers = []
         for a in q.get_answers():
@@ -37,6 +42,61 @@ def quiz_data_view(request, pk):
         'time': quiz.time,
     })
 
+# Guarda los resultados del quiz
 def save_quiz_view(request, pk):
-    print(request.POST)
+    # print(request.POST)
+    if request.is_ajax():
+        questions = []
+        data = request.POST
+        data_ = dict(data.lists())
+        data_.pop('csrfmiddlewaretoken') # Eliminar el token de seguridad
+
+        # Obtener objetos Question
+        for k in data_.keys():
+            print('key:', k)
+            question = Question.objects.get(text=k)
+            questions.append(question)
+
+        user = request.user
+        quiz = Quiz.objects.get(pk=pk)
+
+        # Inicializar variables
+        score = 0
+        multiplier = 100 / quiz.number_of_questions
+        results = []
+        correct_answer = None
+
+        # Recorrer las preguntas
+        for q in questions:
+            a_selected = request.POST.get(str(q.text))
+            
+            if a_selected != "":
+                # Recorrer las respuestas de la pregunta
+                question_answers = Answer.objects.filter(question=q)
+                for a in question_answers:
+                    if a_selected == a.text:
+                        if a.correct:
+                            score += 1
+                            correct_answer = a.text
+                        else:
+                            score -= 1
+                    else:
+                        if a.correct:
+                            correct_answer = a.text
+                # Guardar el resultado de la pregunta
+                results.append({str(q): {'correct_answer': correct_answer, 'answered': a_selected}})
+            else:
+                # Guardar el resultado también si no se seleccionó ninguna respuesta
+                results.append({str(q): 'not answered'})
+
+        # Calcular el puntaje final
+        score_ = score * multiplier
+        Result.objects.create(quiz=quiz, user=user, score=score_)
+
+        # Retornar el resultado del quiz
+        if score_ >= quiz.required_score_to_pass:
+            return JsonResponse({'passed': True, 'score': score_, 'results': results}) # Retorna True si el usuario pasó el quiz
+        else:
+            return JsonResponse({'passed': False, 'score': score_, 'results': results}) # Retorna False si el usuario no pasó el quiz
+
     return JsonResponse({'text': 'works'})
