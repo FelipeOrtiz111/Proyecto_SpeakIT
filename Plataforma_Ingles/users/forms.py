e30481f9a5e8d7b81a0c19e84c12025f9eba051a
Crissxs <cri.carrasco@duocuc.cl>
2024-11-23 23:28:55 -0300

    Cambio Idioma Formularios

--- Plataforma_Ingles/users/forms.py
+++ Plataforma_Ingles/users/forms.py
@@ -4,25 +4,47 @@ from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from .models import CustomUser
 
+# Formulario de Registro de Usuario
 class CustomUserRegistrationForm(UserCreationForm):
-    username = forms.CharField(help_text='150 caracteres o menos. Letras, digitos y solo @/,/+/-/_.', required=True, label="Nombre de usuario")
-    email = forms.EmailField(help_text='Debe ingresar su dirrección de correo institucional de DuocUC.', required=True, label="Correo electrónico")
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+    username = forms.CharField(
+        help_text='150 caracteres o menos. Letras, dígitos y solo @/./+/-/_ permitidos.',
+        required=True,
+        label="Nombre de usuario"
+    )
+    email = forms.EmailField(
+        help_text='Debe ingresar su dirección de correo institucional de DuocUC.',
+        required=True,
+        label="Correo electrónico"
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
         help_text='Tu contraseña no debe ser muy parecida a tus otros datos personales.\
             <br>Tu contraseña debe contener al menos 8 caracteres.\
             <br>Tu contraseña no debe ser muy común.\
-            <br>Tu contraseña no debe ser totalmente numérica.', required=True, label="Contraseña")
-    
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}),
-        help_text='Vuelve a ingresar la contraseña, para verificación.', required=True, label="Confirmar Contraseña")    
+            <br>Tu contraseña no debe ser totalmente numérica.',
+        required=True,
+        label="Contraseña"
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        help_text='Vuelve a ingresar la contraseña, para verificación.',
+        required=True,
+        label="Confirmar Contraseña"
+    )
 
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password1', 'password2']
+        labels = {
+            'username': 'Nombre de usuario',
+            'email': 'Correo Electrónico',
+            'password1': 'Contraseña',
+            'password2': 'Confirmar Contraseña',
+        }
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
-        dominio_permitido = ['duocuc.cl', 'profesor.duoc.cl']  # Dominios duoc
+        dominio_permitido = ['duocuc.cl', 'profesor.duoc.cl']  # Dominios permitidos
         if not any(email.endswith(f"@{dominio}") for dominio in dominio_permitido):
             raise ValidationError(f'Debes ingresar tu correo institucional.')
         return email
@@ -42,31 +64,41 @@ class CustomUserRegistrationForm(UserCreationForm):
             user.save()
         return user
 
-
+# Formulario de Inicio de Sesión
 class UserLoginForm(AuthenticationForm):
-    def __init__(self, *args, **kwargs):
-        super(UserLoginForm, self).__init__(*args, **kwargs)
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombre de usuario o correo Duoc'}),
+        label="Usuario o Correo Institucional"
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Contraseña'}),
+        label="Contraseña"
+    )
 
-    username = forms.CharField(widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': 'Nombre de usuario o correo Duoc'}),
-        label="Usuario o Correo Institucional")
-
-    password = forms.CharField(widget=forms.PasswordInput(
-        attrs={'class': 'form-control', 'placeholder': 'Contraseña'}),
-        label="Contraseña")
-
+# Formulario de Actualización de Usuario
 class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField()
+    email = forms.EmailField(required=True, label="Correo Electrónico")
 
     class Meta:
         model = CustomUser
         fields = ['first_name', 'last_name', 'email', 'description']
-        
-class SetPasswordForm(SetPasswordForm):
+        labels = {
+            'first_name': 'Nombre',
+            'last_name': 'Apellido',
+            'email': 'Correo Electrónico',
+            'description': 'Descripción',
+        }
+
+# Formulario de Cambio de Contraseña
+class CustomSetPasswordForm(SetPasswordForm):
     class Meta:
         model = CustomUser
         fields = ['new_password1', 'new_password2']
-        
-class PasswordResetForm(PasswordResetForm):
-    def __init__(self, *args, **kwargs):
-        super(PasswordResetForm, self).__init__(*args, **kwargs)
+        labels = {
+            'new_password1': 'Nueva Contraseña',
+            'new_password2': 'Confirmar Nueva Contraseña',
+        }
+
+# Formulario de Restablecimiento de Contraseña
+class CustomPasswordResetForm(PasswordResetForm):
+    email = forms.EmailField(label="Correo Electrónico", required=True)
