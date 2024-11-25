b48cc1265124acde87720201016565f8dd137941
FelipeOrtiz111 <101360596+FelipeOrtiz111@users.noreply.github.com>
2024-11-25 10:36:38 -0300

    corrección dashboard progreso individual

--- Plataforma_Ingles/Pagina_Web_Ingles/static/scripts/teacher-dashboard.js
+++ Plataforma_Ingles/Pagina_Web_Ingles/static/scripts/teacher-dashboard.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function() {
             labels: Array.from({ length: Math.max(...students.map(s => dashboardData.student_progress[s].scores.length)) }, (_, i) => `Intento ${i + 1}`),
             datasets: students.map(student => ({
                 label: student,
-                data: [...dashboardData.student_progress[student].scores].reverse(),
+                data: dashboardData.student_progress[student].scores,
                 borderColor: getRandomColor(),
                 fill: false
             }))
