52fecba595da8dc96336b684b4db66b56969aec1
FelipeOrtiz111 <101360596+FelipeOrtiz111@users.noreply.github.com>
2024-12-12 17:13:25 -0300

    Corrección url sección

--- Plataforma_Ingles/users/urls.py
+++ Plataforma_Ingles/users/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path("password_change", views.password_change, name="password_change"),
     path("password_reset", views.password_reset_request, name="password_reset"),
     path('reset/<uidb64>/<token>', views.passwordResetConfirm, name='password_reset_confirm'),
-    path('update_section/', views.update_section, name='update_section'),
+    path('update_section', views.update_section, name='update_section'),
 ]
